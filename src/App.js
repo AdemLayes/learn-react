cfeef7dfa15204795b56afd97c8e25cbf7e9c852
Adem Layes <ademlayesjr@gmail.com>
2023-07-02 00:41:10 +0100

    03/13 learning react
    learn about JSX, components and basic usage of props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 
 export let person = {
   name: "Adem",
@@ -13,12 +14,47 @@ export let person = {
     newest: "Dulipa"
   }
 }
-export default function App({ name, age, music: {old} }) {
+export default function App({ name, age: userAge, music: {old}, pLang = "React" }) {
   return (
-    <ul>
+    <ul> <b>User info</b>
       <li>User name is : {name}</li>
-      <li>User age is : {age}</li>
+      <li>User age is : {userAge}</li>
       <li>User favorite old music is : {old}</li>
+      <li>User is learning : {pLang}</li>
     </ul>
+  )
+}
+// Regular JSX variable
+export let JsxTxt = (
+  <div>
+    <h2>JSX</h2>
+    <p>This is a <i>JSX</i></p>
+    <p>Its cool right ? :)</p>
+  </div>
+);
+// Class Component
+export class ClassCar extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h2>Class component</h2>
+        <p>This is a <i>class component</i>.</p>
+        <p>This is props: {this.props.name}</p>
+      </div>
+    )
+  }
+}
+// Function Component
+export function FunCar(props) {
+  return (
+    <div>
+      <h2>Function component</h2>
+      <p>This is a <i>Function component</i>.</p>
+      <p>This is props: {props.name.length > 3? "Valid": "invalid"}</p>
+      <ClassCar name="Ayoub" />
+    </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import * as all from './App';
-
+import App, {person, JsxTxt, FunCar, ClassCar} from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.StrictMode>
   <>
-    {all.default(all.person)}
+    {App(person)}
+    {JsxTxt}
+    <ClassCar name="Noh"/>
+    <FunCar name="Adem" color="Red"/>
   </>
+  </React.StrictMode>
 );
+
+// ' <></> ' are called fragment
