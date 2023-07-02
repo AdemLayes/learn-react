64ed9ab17ad001e515f91fa7240e994947c72cc8
Adem Layes <ademlayesjr@gmail.com>
2023-07-02 22:53:56 +0100

    04/13 learning react
    learned deeply about 'class component' and 'props'

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export let person = {
   name: "Adem",
   age: 22,
   lang: "Arabic",
-  hoby: [
+  hobby: [
     "coding",
     'vedio game'
   ],
@@ -14,13 +14,14 @@ export let person = {
     newest: "Dulipa"
   }
 }
-export default function App({ name, age: userAge, music: {old}, pLang = "React" }) {
+export default function App({ name, age: userAge, hobby: [firsthobby], music: {old}, pLang = "React" }) {
   return (
     <ul> <b>User info</b>
-      <li>User name is : {name}</li>
-      <li>User age is : {userAge}</li>
-      <li>User favorite old music is : {old}</li>
-      <li>User is learning : {pLang}</li>
+      <li>User name is : { name }</li>
+      <li>User age is : { userAge }</li>
+      <li>User favorite old music is : { old }</li>
+      <li>User first hobby is : { firsthobby }</li>
+      <li>User is learning : { pLang }</li>
     </ul>
   )
 }
@@ -31,30 +32,48 @@ export let JsxTxt = (
     <p>This is a <i>JSX</i></p>
     <p>Its cool right ? :)</p>
   </div>
-);
+)
 // Class Component
 export class ClassCar extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      type: "Civilian car"
+    }
+  }
+  change = () => {
+    if(this.state.type == "Civilian car") {
+      this.setState({
+        type: "Army car"
+      });
+    }else{
+      this.setState({
+        type: "Civilian car"
+      });
+    }
   }
   render() {
     return (
       <div>
         <h2>Class component</h2>
-        <p>This is a <i>class component</i>.</p>
-        <p>This is props: {this.props.name}</p>
+        <p>Using props: { this.props.name }</p>
+        <p>Using state: { this.state.type }</p>
+        <button type="button" onClick={ this.change }>Change</button>
       </div>
     )
   }
 }
 // Function Component
+function PassedFun(props) {
+  return (<p>this is passed component with a prop named : { props.prop }</p>)
+}
 export function FunCar(props) {
+  let propName = "Vanoss"
   return (
     <div>
       <h2>Function component</h2>
-      <p>This is a <i>Function component</i>.</p>
-      <p>This is props: {props.name.length > 3? "Valid": "invalid"}</p>
-      <ClassCar name="Ayoub" />
+      <p>Using props (with ternary operator): { props.name.length > 3? "Valid user name": "invalid user name" }</p>
+      <PassedFun prop={ propName } />
     </div>
   )
 }
